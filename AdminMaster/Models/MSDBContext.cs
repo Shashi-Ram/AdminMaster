d31619717f07ab2df2efb984ec13a6de34449577
Shashi Ram <bharramshashi@outlook.com>
2022-03-13 19:55:48 +0530

    Add project files.

---
+++ AdminMaster/Models/MSDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminMaster.Models
+{
+    public class MSDBContext : DbContext
+    {
+        public MSDBContext():base(){}
+        public DbSet<Tbl_Users> Tbl_Users { get; set; }
+        public DbSet<Verified_Account> Verified_Account { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DBConnection.ConnectionStr);
+        }
+        
+        
+    }
+}
